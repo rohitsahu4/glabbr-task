f50f62790cb0961c7a5f47c716b2556aec05ae50
Rohit sahu <rohitsahu.4998@gmail.com>
2020-02-14 00:58:33 +0530

    Updates

---
+++ precache-manifest.3f88068ca631c2d1224773114ceb0233.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e60aef9b29531591218a0f65bf4d06e",
+    "url": "/glabbr-task/index.html"
+  },
+  {
+    "revision": "edaacc9ff52fa78df7a9",
+    "url": "/glabbr-task/static/css/main.9946af78.chunk.css"
+  },
+  {
+    "revision": "ab6ea8301225a99851d4",
+    "url": "/glabbr-task/static/js/2.b2f84329.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/glabbr-task/static/js/2.b2f84329.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edaacc9ff52fa78df7a9",
+    "url": "/glabbr-task/static/js/main.2ba9ec22.chunk.js"
+  },
+  {
+    "revision": "a38b8dc8f0b429d4ee1d",
+    "url": "/glabbr-task/static/js/runtime-main.0a3cf0f3.js"
+  },
+  {
+    "revision": "268e89298b9317286fce66edec9efcea",
+    "url": "/glabbr-task/static/media/Segoe UI Bold Italic.268e8929.woff"
+  },
+  {
+    "revision": "52382539737f4e9913e4bf6b9966bee3",
+    "url": "/glabbr-task/static/media/Segoe UI Bold.52382539.woff"
+  },
+  {
+    "revision": "347c3ac06f20b6a969fc55e89b37c9c4",
+    "url": "/glabbr-task/static/media/Segoe UI Italic.347c3ac0.woff"
+  },
+  {
+    "revision": "9a2931180d6b1dc7b33052657eef554b",
+    "url": "/glabbr-task/static/media/Segoe UI.9a293118.woff"
+  },
+  {
+    "revision": "3a3dd4201997134be1eb9329c240c363",
+    "url": "/glabbr-task/static/media/futura-pt-book-589a6dec272c3.3a3dd420.woff"
+  }
+]);
